fc8bc69f0b6ee44cabd1c216f84f32ba3e1de62f
True1023 <752602742@qq.com>
2018-07-09 23:34:34 +0800

    v 0.0.4
    weather实现了部分功能
    整理了文件夹结构

---
+++ wechat_auto/components/weather.py
@@ -0,0 +1,157 @@
+import requests
+import json
+
+'''
+UNDO, 需要定时获取当天所有天气的预报数据，保存到本地，之后通过调用则返回本地数据
+'''
+
+KEY = 'jpc0si7sh9qjudrz'  # API key
+UID = "U396147DF7"  # 用户ID
+NOW_API = 'https://api.seniverse.com/v3/weather/now.json'  # API URL，可替换为其他 URL
+DAILY_API = 'https://api.seniverse.com/v3/weather/daily.json'
+LIFE_API = 'https://api.seniverse.com/v3/life/suggestion.json'
+UNIT = 'c'  # 单位
+LANGUAGE = 'zh-Hans'  # 查询结果的返回语言
+
+def _now(location):
+    result = requests.get(NOW_API, params={
+        'key': KEY,
+        'location': location,
+        'language': LANGUAGE,
+        'unit': UNIT
+    }, timeout=1)
+    result = json.loads(result.text)
+    if 'results' in result:
+        return (True, location+'当前天气'+result['results'][0]['now']['text']+", 气温"+result['results'][0]['now']['temperature']+"摄氏度")
+    return (False,result['status'])
+
+def _daily(location):
+    result = requests.get(DAILY_API, params={
+        'key': KEY,
+        'location': location,
+        'language': LANGUAGE,
+        'unit': UNIT
+    }, timeout=1)
+    result = json.loads(result.text)
+    return result
+
+def _life(location):
+    result = requests.get(LIFE_API, params={
+        'key': KEY,
+        'location': location,
+        'language': LANGUAGE,
+    }, timeout=1)
+    result = json.loads(result.text)
+    return result
+
+def _all(location):
+    result = {}
+    result['daily'] = _daily(location)
+    result['life'] = _life(location)
+    return result
+
+#获取目的城市的天气简报
+def brief(location):
+    content = _all(location)
+    result = ""
+    #如果查询出结果
+    if 'results' in content['life']:
+        today = content['daily']['results'][0]['daily'][0]
+        suggestion = content['life']['results'][0]['suggestion']
+        result = '%s今天(%s)白天%s,夜间%s,最高温度%s摄氏度,最低温度%s摄氏度,%s风。%s洗车，天气偏%s,%s运动，%s旅行' % (location, today['date'], today['text_day'], today['text_night'], today['high'], today['low'], today['wind_direction'],suggestion['car_washing']['brief'],suggestion['dressing']['brief'],suggestion['sport']['brief'],suggestion['travel']['brief'])
+        return (True, result)
+    else:#如果没有
+        return (False, content['life']['status'])
+
+
+
+
+'''Now formate
+{
+    "results": [
+        {
+            "location": {
+                "id": "WX4FBXXFKE4F",
+                "name": "北京",
+                "country": "CN",
+                "path": "北京,北京,中国",
+                "timezone": "Asia/Shanghai",
+                "timezone_offset": "+08:00"
+            },
+            "now": {
+                "text": "多云",
+                "code": "4",
+                "temperature": "25"
+            },
+            "last_update": "2018-07-09T20:25:00+08:00"
+        }
+    ]
+}
+
+{
+    "status": "The location can not be found.",
+    "status_code": "AP010010"
+}
+'''
+
+'''
+{
+    "results": [
+        {
+            "location": {
+                "id": "WX4FBXXFKE4F",
+                "name": "北京",
+                "country": "CN",
+                "path": "北京,北京,中国",
+                "timezone": "Asia/Shanghai",
+                "timezone_offset": "+08: 00"
+            },
+            "daily": [
+                {
+                    "date": "2018-07-09",
+                    "text_day": "雷阵雨",
+                    "code_day": "11",
+                    "text_night": "阴",
+                    "code_night": "9",
+                    "high": "29",
+                    "low": "22",
+                    "precip": "",
+                    "wind_direction": "东南",
+                    "wind_direction_degree": "135",
+                    "wind_speed": "10",
+                    "wind_scale": "2"
+                },
+                {
+                    "date": "2018-07-10",
+                    "text_day": "多云",
+                    "code_day": "4",
+                    "text_night": "小雨",
+                    "code_night": "13",
+                    "high": "30",
+                    "low": "22",
+                    "precip": "",
+                    "wind_direction": "南",
+                    "wind_direction_degree": "180",
+                    "wind_speed": "10",
+                    "wind_scale": "2"
+                },
+                {
+                    "date": "2018-07-11",
+                    "text_day": "小雨",
+                    "code_day": "13",
+                    "text_night": "小雨",
+                    "code_night": "13",
+                    "high": "25",
+                    "low": "22",
+                    "precip": "",
+                    "wind_direction": "南",
+                    "wind_direction_degree": "180",
+                    "wind_speed": "10",
+                    "wind_scale": "2"
+                }
+            ],
+            "last_update": "2018-07-09T11: 00: 00+08: 00"
+        }
+    ]
+}
+'''
